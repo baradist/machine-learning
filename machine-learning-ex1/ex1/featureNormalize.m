25de4e902807c58449cdc85c2877eb8b098cbf09
Oleg Grigoryev <oleg_grigoryev@epam.com>
2018-01-28 18:59:36 +0300

    featureNormalize.m

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -25,14 +25,14 @@ sigma = zeros(1, size(X, 2));
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
+n = size(X, 2);
+for i=1:n
+  mu(1, i) = mean(X(:,i));
+  sigma(1, i) = std(X(:,i));
+  X_norm(:,i) = (X(:,i) - mu(1, i)) / sigma(1, i);
+end;
 
-
-
-
-
-
-
-
+% disp(X_norm(1:10, :)); % debug
 
 % ============================================================
 
